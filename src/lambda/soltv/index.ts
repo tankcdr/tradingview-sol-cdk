9025e57c571cc5791a60448fe9016d120829f3ed
Christopher Madison <chris_madison@bellsouth.net>
2025-02-20 07:38:28 -0500

    adding debug statements

--- src/lambda/soltv/index.ts
+++ src/lambda/soltv/index.ts
@@ -327,7 +327,7 @@ export const handler = async (event: any) => {
         trade = await executeTrade(USDC_MINT, SOL_MINT, usdcBalance);
         await updateState("BUY");
       } else {
-        console.log("Insufficient USDC.");
+        console.error("Insufficient USDC.");
         return {
           statusCode: 400,
           body: JSON.stringify({
@@ -344,6 +344,7 @@ export const handler = async (event: any) => {
         trade = await executeTrade(SOL_MINT, USDC_MINT, amountToSwap);
         await updateState("SELL");
       } else {
+        console.error("Insufficient SOL.");
         return {
           statusCode: 400,
           body: JSON.stringify({
